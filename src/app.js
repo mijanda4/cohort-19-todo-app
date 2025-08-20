734ddba718eb2e11f2c7cc802469d0d7629cfda7
MIJanda <mokiliisaacjanda@gmail.com>
2025-08-21 00:05:35 +0300

    initial frontend scaffolding

---
+++ src/app.js
@@ -0,0 +1,5 @@
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css'
+// Import Bootstrap JS bundle (includes Popper)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
